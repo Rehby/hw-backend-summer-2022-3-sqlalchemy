54fcebe095184c20c0795fa3e064dbcabacbeec9
Rehby <59175059+Rehby@users.noreply.github.com>
2023-02-27 16:05:03 +0500

    test

---
+++ alembic/versions/5402fbfd8754_tables.py
@@ -0,0 +1,24 @@
+"""tables
+
+Revision ID: 5402fbfd8754
+Revises: 
+Create Date: 2023-02-27 15:59:41.395165
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5402fbfd8754'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
